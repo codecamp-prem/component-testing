7c5c7c49ed3acf7b30317cd6a5265233fece485c
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-05 11:44:07 +0545

    Always remember useEvent.click that submit function will check for validation also if any is present for form. so be sure to pass valid data in userEvent.type. Miss Rust!! Then only expect(onSubmit).toHaveCalledOnce() or toHaveBeenCalledWith({data, data}) will work other wise very verbose error.

--- setupTest.js
+++ setupTest.js
@@ -1,9 +1,9 @@
-import { cleanup } from "@testing-library/react";
-import { expect } from 'vitest';
-import matchers from '@testing-library/jest-dom/matchers';
+import { afterEach, expect } from "vitest"
+import matchers from "@testing-library/jest-dom/matchers"
+import { cleanup } from "@testing-library/react"
 
 expect.extend(matchers)
 
 afterEach(() => {
-    cleanup()
+  cleanup()
 })
--- src/Form.test.jsx
+++ src/Form.test.jsx
@@ -1,21 +1,24 @@
 import { describe, expect, it, vi } from "vitest"
 import { StateForm as Form } from "./StateForm"
 import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
+import userEvent from '@testing-library/user-event'
 
 describe("Form Component", () => {
     it("should call onSubmit when the form is valid with the correct data", async () => {
-        const user = userEvent.setup()
-
         const onSubmit = vi.fn()
-        render(<Form onSubmit={onSubmit}/>)
+        const user = userEvent.setup()
+        
+        render(<Form onSubmit={onSubmit} />)
 
         const email = "test@gmail.com"
-        const password = "123456"
+        const password = "123456Password"
 
         await user.type(screen.getByLabelText("Email"), email)
         await user.type(screen.getByLabelText("Password"), password)
         await user.click(screen.getByText("Submit"))
+        
+        expect(screen.queryByTestId("email-error-msg")).not.toBeInTheDocument()
+        expect(screen.queryByTestId("password-error-msg")).not.toBeInTheDocument()
 
         expect(onSubmit).toHaveBeenCalledOnce()
         expect(onSubmit).toHaveBeenCalledWith({ email, password })
